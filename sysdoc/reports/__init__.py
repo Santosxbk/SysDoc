40c0262cefd817f6ff90faab7d42e78926118b4f
Santos <dekod8270@gmail.com>
2026-09-09 16:58:01 +0000

    Fix package export and ignore rules for reports module

---
+++ sysdoc/reports/__init__.py
@@ -0,0 +1,5 @@
+"""Reporting and export helpers for SysDoc."""
+
+from .exporters import export_html_report, export_json_report, export_text_report
+
+__all__ = ["export_text_report", "export_json_report", "export_html_report"]
